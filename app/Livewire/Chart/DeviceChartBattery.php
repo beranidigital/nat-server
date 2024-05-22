e80573373172afa58e6def79c6bf91700ce800b9
Your Name <dedenkoesuma72@gmail.com>
2024-05-22 18:01:39 +0700

    fixbugs: fix filters-chart

--- app/Livewire/Chart/DeviceChartBattery.php
+++ app/Livewire/Chart/DeviceChartBattery.php
@@ -40,7 +40,7 @@ class DeviceChartBattery extends ChartWidget
     protected function getData(): array
     {
         $filters = ChartPoolDetail::extractFilter($this->filters);
-        $startDate = $filters['startDate'] ?? now()->subDays(5);
+        $startDate = $filters['startDate'] ?? now()->subDay();
         $endDate = $filters['endDate'] ?? now();
         $frequency = $filters['frequency'] ?? 'Daily';
 
@@ -55,7 +55,7 @@ class DeviceChartBattery extends ChartWidget
         return [
             'datasets' => [
                 [
-                    'label' => 'Chlorine',
+                    'label' => 'Battery',
                     'data' => $battery['data'],
                 ],
             ],
